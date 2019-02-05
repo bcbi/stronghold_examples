cb4b969aece1bdbb8360dd0814d576b9db7d3ed9
Isabel Restrepo <isabelrestre@gmail.com>
2019-02-05 09:51:48 -0500

    update ctakes example

--- ctakes/ctakes_cli.sh
+++ ctakes/ctakes_cli.sh
@@ -1,9 +1,9 @@
 CTAKES_HOME=/opt/apache-ctakes-4.0.0
 
-OUTDIR="./clinical_notes_output"
+OUTDIR="$HOME/stronghold_examples/ctakes/clinical_notes_output"
 
 if [ ! -d "$OUTDIR" ]; then
   mkdir $OUTDIR
 fi
 
-${CTAKES_HOME}/bin/runClinicalPipeline.sh  -i ./clinical_notes_input --xmiOut $OUTDIR -l org/apache/ctakes/dictionary/lookup/fast/sno_rx_18ab.xml
+${CTAKES_HOME}/bin/runClinicalPipeline.sh  -i $HOME/stronghold_examples/ctakes/clinical_notes_input --xmiOut $OUTDIR -l org/apache/ctakes/dictionary/lookup/fast/sno_rx_18ab_sh.xml
